e32b31e751705664a883409cb97cb2e6c6e3f173
ciroimpa <104090636+ciroimpa@users.noreply.github.com>
2022-05-26 16:54:09 +0200

    aa
    aaa

--- location_map/function.c
+++ location_map/function.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define max_length 41
+#define max_length_colonna 5
 
 void reading()
 {
@@ -16,13 +17,27 @@ void reading()
     char *riga_2 = strtok(NULL, "!");
     char *riga_3 = strtok(NULL, "!");
     char *riga_4 = strtok(NULL, "!");
-    printf("%s\n%s\n%s\n%s", riga_1, riga_2, riga_3, riga_4);
-    char token = 1;
-    while (token != NULL)
-    {
-        //printf("%s\n", token);
-        token = strtok(NULL, ",");
-    }
+    char *1_1= strtok (riga_1, ",");
+    char *1_2= strtok (NULL, ",");
+    char *1_3= strtok (NULL, ",");
+    char *1_4= strtok (NULL, ",");
+    char *1_5= strtok (NULL, ",");
+    char *2_1= strtok (riga_2, ",");
+    char *2_2= strtok (NULL, ",");
+    char *2_3= strtok (NULL, ",");
+    char *2_4= strtok (NULL, ",");
+    char *2_5= strtok (NULL, ",");
+    char *3_1= strtok (riga_3, ",");
+    char *3_2= strtok (NULL, ",");
+    char *3_3= strtok (NULL, ",");
+    char *3_4= strtok (NULL, ",");
+    char *3_5= strtok (NULL, ",");
+    char *4_1= strtok (riga_4, ",");
+    char *4_2= strtok (NULL, ",");
+    char *4_3= strtok (NULL, ",");
+    char *4_4= strtok (NULL,",");
+    char *4_5= strtok (NULL, ",");
+    printf
 
 }
 
